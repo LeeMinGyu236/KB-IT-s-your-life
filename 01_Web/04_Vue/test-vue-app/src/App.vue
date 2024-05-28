d55d6f53fc782480530271ce7c817d628fd7ad7b
농구하는 <alsrb85822@gmail.com>
2024-05-29 00:05:16 +0900

    bootstrap설치 및  main.js까지 업데이트

---
+++ 01_Web/04_Vue/test-vue-app/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+export default {
+  name: 'App',
+  component: { CheckboxItem },
+  data() {
+    return {
+      idols: [
+        { id: 1, name: 'bts', checked: true },
+        { id: 2, name: 'black pink', checked: false },
+        { id: 3, name: 'ITZY', checked: false },
+        { id: 4, name: 'EXO', checked: false },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h3>관심있는 KPOP 가수?</h3>
+    <hr />
+    <ul>
+      <CheckboxItem
+        v-for="idol in idols"
+        :name="idol.name"
+        :key="idol.checked"
+      />
+    </ul>
+  </div>
+</template>
---
+++ 01_Web/04_Vue/test-vue-app/src/App2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>관심있는 K-pop가수?</h2>
+    <hr />
+    <ul>
+      <CheckboxItem v-for="idol in idols" :key="idol.id" :idol="idol" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import CheckboxItem from './components/CheckboxItem2.vue';
+export default {
+  name: 'App',
+  components: { CheckboxItem },
+  data() {
+    return {
+      idols: [
+        { id: 1, name: 'bts', checked: true },
+        { id: 2, name: 'black pink', checked: false },
+        { id: 3, name: 'ITZY', checked: false },
+        { id: 4, name: 'EXO', checked: false },
+      ],
+    };
+  },
+};
+</script>
